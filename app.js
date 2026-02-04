07dc4fd0ca466db9caeca41cc687d19c04f8dafb
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-02-04 11:49:17 +0000

    added routes.js and updated app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const express = require("express");
 const app = express();
+
+const topicsRouter = require("./routes/routes");
+
+app.use("/api/topics", topicsRouter);
---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+// associate a url with a function
+// handle invalid routes(errors will come tomorrow)
+
+// const express = require('express');
+// const router express.Router(); "/snacks/:snacksid" will be handled by this class
+
+// router.get("/"), (req,res)=>{
+
+// }
+
+const express = require("express");
+const router = express.Router();
+
+(router.get("/"), (req, res) => {});
